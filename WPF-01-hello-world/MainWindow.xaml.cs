225908f10c67fcc270e25cab4d8b5435c35a9434
Jan Koupil <jan.koupil@delta-skola.cz>
2021-10-21 12:19:29 +0200

    Lekce 21.10. WPF

---
+++ WPF-01-hello-world/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_01_hello_world
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private int pocetKliku = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (HelloLabel.Content == "Goodbye World!")
+            {
+                HelloLabel.Content = "Hello World!";
+            }
+            else
+            { 
+                HelloLabel.Content = "Goodbye World!";
+            }
+
+            pocetKliku++;
+            PocetTextBlock.Text = pocetKliku.ToString();
+        }
+
+        private void Reset_Click(object sender, RoutedEventArgs e)
+        {
+            pocetKliku = 0;
+            PocetTextBlock.Text = pocetKliku.ToString();
+        }
+
+    }
+}
---
+++ wpf-02-Scitadlo/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpf_02_Scitadlo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SectiBtn_Click(object sender, RoutedEventArgs e)
+        {
+            double cislo1, cislo2, soucet;
+
+            if (
+                double.TryParse(Input1TB.Text, out cislo1) &&
+                double.TryParse(Input2TB.Text, out cislo2)
+            )
+            {
+                soucet = cislo1 + cislo2;
+                OutputTB.Text = soucet.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Jsi pako, máš tam špatný vstup");
+            }
+        }
+    }
+}
