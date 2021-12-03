f60430a65d480316e0a39aa5a5c011bb0cf842f1
Jan Koupil <jan.koupil@delta-skola.cz>
2021-12-03 08:37:59 +0100

    2021-12-03 Další WPF

---
+++ WPF-11-Kontextove-zakazane-prvky/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_11_Kontextove_zakazane_prvky
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            setDiscountState();
+        }
+
+        private void CheckBox_Toggled(object sender, RoutedEventArgs e)
+        {
+            setDiscountState();
+        }
+
+
+        private void setDiscountState()
+        {
+            if (DiscountCB.IsChecked == true)
+            {
+                DiscountsCTRL.IsEnabled = true;
+            }
+            else
+            {
+                DiscountsCTRL.IsEnabled = false;
+            }
+        }
+    }
+}
---
+++ WPF-12-TextProhlizec/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace WPF_12_TextProhlizec
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void LoadBtn_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog op = new OpenFileDialog();
+            op.Title = "Select text file";
+            op.Filter = "Text files (*.txt)|*.txt";
+            if (op.ShowDialog() == true)
+            {
+                try
+                {
+                    TextDisplay.Text = System.IO.File.ReadAllText(op.FileName);
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Chyba čtení souboru", "Chyba čtení souboru", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+
+            }
+        }
+    }
+}
