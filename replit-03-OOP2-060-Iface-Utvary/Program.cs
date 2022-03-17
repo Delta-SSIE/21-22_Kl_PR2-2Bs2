eb1d83e351ea079a0726bfa594cbd21812662424
Jan Koupil <jan.koupil@gmail.com>
2022-03-17 12:08:38 +0100

    IUtvar - hotovo vše mimo plechovku

--- replit-03-OOP2-060-Iface-Utvary/Program.cs
+++ replit-03-OOP2-060-Iface-Utvary/Program.cs
@@ -11,6 +11,24 @@ namespace replit_03_OOP2_060_Iface_Utvary
             Trojuhelnik t = new Trojuhelnik(3, 4, 5);
             Kruh k = new Kruh(2);
 
+            IUtvar[] utvary = new IUtvar[4];
+            utvary[0] = o;
+            utvary[1] = c;
+            utvary[2] = t;
+            utvary[3] = k;
+
+            double sumObvod = 0;
+            double sumObsah = 0;
+            foreach(IUtvar u in utvary)
+            {
+                Console.WriteLine(u.ToString());
+                sumObvod += u.GetObvod();
+                sumObsah += u.GetObsah();
+            }
+            Console.WriteLine("Obvod celkem: " + sumObvod);
+            Console.WriteLine("Obsah celkem: " + sumObsah);
+
+
         }
 
         static void Popis(IUtvar utvar)
